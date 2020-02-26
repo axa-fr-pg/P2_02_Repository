73f488d5e55009d850b062436a416165840e649d
philippegeyaxa <philippe.gey@axa.fr>
2020-02-26 17:44:45 +0100

    Merge feature data analysis

---
+++ projet_p2/src/com/hemebiotech/analytics/IBioAnalyzer.java
@@ -0,0 +1,34 @@
+package com.hemebiotech.analytics;
+
+/**
+ * Interface for analysis of any kind of biological data
+ * @author Philippe GEY
+ * @version 1.0
+ */
+public interface IBioAnalyzer {
+
+	/**
+	 * Method containing all business intelligence of the biological analysis
+	 * @param symptom
+	 */
+	void createMapEntry(String symptom);
+
+	/**
+	 * Method to create a map of biological data from an input file
+	 */
+	void buildMapFile();
+	
+	/**
+	 * Method for converting a map of biological entries into a String
+	 * @return String of sorted map entries
+	 */
+	String toString();
+	
+	/**
+	 * Method to store a map containing biological data into an output file.
+	 * Entries are written in the format <biological key>=<biological value>.
+	 * Each map entry will be followed by a <line separator>.
+	 */
+	void storeSortedMapFile();
+	
+}
